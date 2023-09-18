ce35b22e12bfe9f42da5981254c92147a7cc374c
J-JIM <86223188+J-JIM@users.noreply.github.com>
2023-09-18 10:49:20 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  count = toggle = 0;
+
+}
+
+void loop() {
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  while(1){
+    Serial.println(++count);
+
+    pinMode(PIN_LED, toggle);
+    toggle = !toggle;
+    delay(200);
+    if(count == 10){
+      break;
+    }
+  }
+
+}
